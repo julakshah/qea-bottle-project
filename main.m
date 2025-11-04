53f87fafe3b80fe873156702c3e71449b62990f1
julakshah <julakshah@gmail.com>
2025-11-04 13:48:34 -0500

    code structure

---
+++ main.m
@@ -0,0 +1,11 @@
+function main()
+    % mass of empty botle 198g
+    % opening of bottle 19mm
+    % neck length 80mm
+    bottleMass = 198; % mass of empty bottle in grams
+    openingDiameter = 19; % opening diameter in mm
+    neckLength = 80; % neck length in mm
+    bottleFullMass = 576
+
+    
+end
